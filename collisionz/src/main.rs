4b35bc82d11f9b9ae4f33203de155494d2716a4e
Maciej Pruchnik <maciej.pruchnik@gmail.com>
2022-11-24 21:38:25 -0500

    [Implementation] Basic setup

--- collisionz/src/main.rs
+++ collisionz/src/main.rs
@@ -1,3 +1,12 @@
+mod game;
+
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
+        .run();
 }
